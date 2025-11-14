c9f375f6f7ffe95000c64f44798dfd7336870729
azebusmc <65170242+azebusmc@users.noreply.github.com>
2025-11-14 11:00:11 -0500

    added event listener for blog posts, error styling, and moved css import to load after bootstrap

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-//get inputs
 let formInp = document.getElementById("submitForm");
 let titleInp = document.getElementById("title");
 let contentInp = document.getElementById("content");
 let submitInp = document.getElementById("submitButton");
+let blogPosts = document.getElementById("blogPosts");
 
 //get local storage
 let storedData;
@@ -24,6 +24,7 @@ contentInp.addEventListener("change", (e) => {
 submitInp.addEventListener("click", (e) => {
   //set validation
 });
+blogPosts.addEventListener("click", updatePost);
 
 //methods
 function submit(e) {
@@ -31,5 +32,17 @@ function submit(e) {
   //run validations
   //If valid, create a new post object (e.g., with id, title, content, timestamp).
   //Add edit button which enforces validation and updated storage
+  //toggle save button when edit is clicked
   //add delete button which updates storage
+}
+
+function addPost(post) {
+  //add logic for a new post here
+}
+
+function updatePost(e) {
+  //add update post logic here
+  //if target.deletebutton then delete
+  //if edit button then edit
+  //if save button then save
 }
